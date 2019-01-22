1ada249ef420cc6c20e7bd10dd4cc3e08dc8cb40
codingchris779 <33784082+codingchris779@users.noreply.github.com>
2019-01-22 07:50:11 -0500

    Added FTC dashboard for extra telemetry
    ----------------------------------------
    Hey look guys can't break anything if Im in an issolated branch

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDF.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDF.java
@@ -1,26 +1,62 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.acmerobotics.dashboard.FtcDashboard;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
 @Autonomous(name = "lol")
 public class PIDF extends LinearOpMode {
+    private DcMotorEx boardEx;
+    private ElapsedTime tim;
 
     @Override
     public void runOpMode() throws InterruptedException {
 
-        DcMotorEx boardEx = (DcMotorEx) hardwareMap.dcMotor.get("clark");
-        Trajectory trajectory = new Trajectory(8f,10f,14f,80f);
-        float[][]traj = trajectory.getTraj();
-        waitForStart();
-        while (opModeIsActive()) {
-           boardEx.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            boardEx.setPower(1);
-           telemetry.addData("VEL", boardEx.getVelocity());
-           telemetry.update();
 
-        }
+        Trajectory trajectory = new Trajectory(2940f,10000f,4567.2f,80f);
+        float[][]traj = trajectory.getTraj();
+        ElapsedTime elMili;
+        waitForStart();
+        elMili= new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
+        while (opModeIsActive()) {
+            boardEx =  (DcMotorEx) hardwareMap.dcMotor.get("clark");
+            boardEx.setPower(1);
+
+
+            FtcDashboard dashboard = FtcDashboard.getInstance();
+            Telemetry dashboardTelemetry = dashboard.getTelemetry();
+
+            dashboardTelemetry.addData("Vel", boardEx.getVelocity());
+            dashboardTelemetry.addData("Projected Vel",traj[(int)elMili.time()/1000][1]);
+            dashboardTelemetry.addData("time",elMili.time());
+            dashboardTelemetry.update();
+ }
     }
+//    public double findMaxAccel(){
+//        tim = new ElapsedTime();
+//        //boardEx.setPower(1);
+//        double startTime = 0;
+//        double finTime = 0;
+//        double startVel =0;
+//        double finVel = 0;
+//        double maxAccel = 0;
+//        double Accel;
+//        while (tim.time()<1000){
+//            startTime = tim.time();
+//          startVel =  boardEx.getVelocity();
+//          sleep(10);
+//          finVel = boardEx.getVelocity();
+//          finTime = tim.time();
+//             Accel = (finVel - startVel) / (finTime - startTime);
+//             if(maxAccel<Accel)
+//                 maxAccel= Accel;
+//        }
+//        return maxAccel;
+//    }
 
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotValues.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.dashboard.config.Config;
+
+@Config
+public class RobotValues {
+    public static int MAGIC_NUMBER = 32;
+}
