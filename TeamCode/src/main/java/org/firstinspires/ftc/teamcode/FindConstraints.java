b086044830f1a28e52aa47d5d7d2d30a86828268
codingchris779 <33784082+codingchris779@users.noreply.github.com>
2019-02-14 11:57:47 -0500

    PIDF is broken. It is getting stuck in loop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FindConstraints.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FindConstraints.java
@@ -1,13 +1,18 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
-public class FindConstraints extends LinearOpMode {
+import static java.lang.Thread.sleep;
+
+@TeleOp (name = "finding PID constraints")
+public class FindConstraints extends OpMode {
 public DcMotorEx frontLeft;
     private int starto;
     private int endo;
@@ -15,13 +20,21 @@ public DcMotorEx frontLeft;
     private double maxVel=0;
     private double acc=0;
     private double maxAcc=0;
+    ElapsedTime elTiempo;
+
 
     @Override
-    public void runOpMode() throws InterruptedException {
-        frontLeft = (DcMotorEx) hardwareMap.get("frontLeft");
+    public void init() {
+        frontLeft = (DcMotorEx) hardwareMap.get("extendy");
+
+        elTiempo = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
+    }
+
+    @Override
+    public void loop()  {
         frontLeft.setPower(1);
-        ElapsedTime elTiempo = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
-        while (opModeIsActive()){
+
+
             starto = (int)elTiempo.time();
             if(frontLeft.getVelocity()>maxVel)
                 maxVel = frontLeft.getVelocity();
@@ -30,8 +43,15 @@ public DcMotorEx frontLeft;
                 maxAcc=acc;
             endo = (int)elTiempo.time();
             oldspeed = frontLeft.getVelocity();
-            telemetry.addData("maxVel",maxVel);
+            telemetry.addData("maxVel",frontLeft.getVelocity());
             telemetry.addData("maxAcc",maxAcc);
+            telemetry.addData("position", frontLeft.getCurrentPosition());
+            telemetry.update();
+        try {
+            sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-        }
+
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDF.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDF.java
@@ -1,5 +1,5 @@
 package org.firstinspires.ftc.teamcode;
-
+import org.firstinspires.ftc.teamcode.RobotValues;
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -7,13 +7,6 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
- class MotorControl extends Thread{
-     @Override
-     public void run() {
-
-        // super.run();
-     }
- }
 
 @Autonomous(name = "lol")
 public class PIDF extends LinearOpMode {
@@ -27,13 +20,13 @@ public class PIDF extends LinearOpMode {
     private double start;
     private int startEncoderPosition;
     private float positionError;
+
     public static float handyDandyPosPID(float positionError, float PGain){
         return positionError*PGain;
     }
 
     @Override
-
-    public void runOpMode() {
+    public synchronized void runOpMode() {
         FtcDashboard dashboard = FtcDashboard.getInstance();
         Telemetry dashboardTelemetry = dashboard.getTelemetry();
         Trajectory trajectory = new Trajectory((320f)/4f, 511f, (6400f)/4f, 80f);
@@ -49,74 +42,68 @@ public class PIDF extends LinearOpMode {
         start = elMili.time();
         startEncoderPosition = frontLeft.getCurrentPosition();
         while(opModeIsActive()) {
-            if(RobotValues.WTFAREWEEVENDOING == 0){
-                telemetry.addData("What do you want to do","woo");
-                telemetry.update();
-                start = elMili.time();
-                dashboardTelemetry.addData("frontLeft Speeed", frontLeft.getVelocity());
-//                dashboardTelemetry.addData("frontRight",frontRight.getVelocity());
-//                dashboardTelemetry.addData("backLeft",backLeft.getVelocity());
-//                dashboardTelemetry.addData( "backRight",backRight.getVelocity());
-//                dashboardTelemetry.addData("backRight pos", backRight.getCurrentPosition());
-//                dashboardTelemetry.addData("backRight pos", backLeft.getCurrentPosition());
-//                dashboardTelemetry.addData("backRight pos", frontRight.getCurrentPosition());
-//                dashboardTelemetry.addData("backRight pos", frontRight.getCurrentPosition());
-//                dashboardTelemetry.addData("What it should be ", speed);
-                dashboardTelemetry.addData("Velocity error",speed-frontLeft.getVelocity());
-                dashboardTelemetry.update();
-            }if(RobotValues.WTFAREWEEVENDOING==1){
-            while ((int)(elMili.time())-start<traj.length) {
-                //positionError = startEncoderPosition - traj[(int) (elMili.time() - start)][2];
-                // Set some cool pid values
-                frontLeft.setVelocityPIDFCoefficients(RobotValues.KV, 0, 0, RobotValues.FEED_FORWARD * (traj[(int) (elMili.time() - start + 5)][1]));
-//                frontRight.setVelocityPIDFCoefficients(RobotValues.KV, 0, 0, RobotValues.FEED_FORWARD * (traj[(int) (elMili.time() - start + 5)][1]));
-//                backLeft.setVelocityPIDFCoefficients(RobotValues.KV, 0, 0, RobotValues.FEED_FORWARD * (traj[(int) (elMili.time() - start + 5)][1]));
-//                frontLeft.setVelocityPIDFCoefficients(RobotValues.KV, 0, 0, RobotValues.FEED_FORWARD * (traj[(int) (elMili.time() - start + 5)][1]));
-                dashboardTelemetry.addData("frontLeft Speeed", frontLeft.getVelocity());
-//                dashboardTelemetry.addData("frontRight",frontRight.getVelocity());
-//                dashboardTelemetry.addData("backLeft",backLeft.getVelocity());
-//               dashboardTelemetry.addData( "backRight",backRight.getVelocity());
-//               dashboardTelemetry.addData("backRight pos", backRight.getCurrentPosition());
-                dashboardTelemetry.addData("What it should be ", speed);
-                dashboardTelemetry.addData("Velocity error",speed-frontLeft.getVelocity());
-                dashboardTelemetry.update();
 
-                speed = traj[(int) (elMili.time() - start)][1];
-                telemetry.addData("velocity", speed);
-                telemetry.update();
-              //  frontRight.setVelocity(-speed);
-                frontLeft.setVelocity(speed);
-                //backRight.setVelocity(-speed);
-                //backLeft.setVelocity(speed);
-            }
-            start = elMili.time();
-        }
-            if(RobotValues.WTFAREWEEVENDOING==2){
-                while ((int)(elMili.time())-start<traj.length) {
-                    positionError = startEncoderPosition - traj[(int) (elMili.time() - start)][2];
-                    // Set some cool pid values
-                    frontLeft.setVelocityPIDFCoefficients(RobotValues.KV, 0, 0, RobotValues.FEED_FORWARD * (traj[(int) (elMili.time() - start + 5)][1]));
-//                    frontRight.setVelocityPIDFCoefficients(RobotValues.KV, 0, 0, RobotValues.FEED_FORWARD * (traj[(int) (elMili.time() - start + 5)][1]));
-//                    backLeft.setVelocityPIDFCoefficients(RobotValues.KV, 0, 0, RobotValues.FEED_FORWARD * (traj[(int) (elMili.time() - start + 5)][1]));
+//                telemetry.addData("What do you want to do", "woo");
+//                telemetry.update();
+//                start = elMili.time();
+//                dashboardTelemetry.addData("frontLeft Speeed", frontLeft.getVelocity());
+//                dashboardTelemetry.addData("Velocity error", speed - frontLeft.getVelocity());
+//                dashboardTelemetry.update();
+
+//            if(RobotValues.WTFAREWEEVENDOING==1&& elMili.time()-start<traj.length){
+//                while ((int)(elMili.time())-start<traj.length) {
+//                    //positionError = startEncoderPosition - traj[(int) (elMili.time() - start)][2];
+//                    // Set some cool pid values
 //                    frontLeft.setVelocityPIDFCoefficients(RobotValues.KV, 0, 0, RobotValues.FEED_FORWARD * (traj[(int) (elMili.time() - start + 5)][1]));
-                    dashboardTelemetry.addData("frontLeft Speeed", frontLeft.getVelocity());
-                    dashboardTelemetry.addData("What it should be ", speed);
-                    dashboardTelemetry.addData("Velocity error",speed-frontLeft.getVelocity());
-                    dashboardTelemetry.update();
+////                frontRight.setVelocityPIDFCoefficients(RobotValues.KV, 0, 0, RobotValues.FEED_FORWARD * (traj[(int) (elMili.time() - start + 5)][1]));
+////                backLeft.setVelocityPIDFCoefficients(RobotValues.KV, 0, 0, RobotValues.FEED_FORWARD * (traj[(int) (elMili.time() - start + 5)][1]));
+////                frontLeft.setVelocityPIDFCoefficients(RobotValues.KV, 0, 0, RobotValues.FEED_FORWARD * (traj[(int) (elMili.time() - start + 5)][1]));
+//                    dashboardTelemetry.addData("frontLeft Speeed", frontLeft.getVelocity());
+////                dashboardTelemetry.addData("frontRight",frontRight.getVelocity());
+////                dashboardTelemetry.addData("backLeft",backLeft.getVelocity());
+////               dashboardTelemetry.addData( "backRight",backRight.getVelocity());
+////               dashboardTelemetry.addData("backRight pos", backRight.getCurrentPosition());
+//                    dashboardTelemetry.addData("What it should be ", speed);
+//                    dashboardTelemetry.addData("Velocity error",speed-frontLeft.getVelocity());
+//                    dashboardTelemetry.update();
+//
+//                    speed = traj[(int) (elMili.time() - start)][1];
+//                    telemetry.addData("velocity", speed);
+//                    telemetry.update();
+//                    //  frontRight.setVelocity(-speed);
+//                    frontLeft.setVelocity(speed);
+//                    //backRight.setVelocity(-speed);
+//                    //backLeft.setVelocity(speed);
+//                }
+//                start = elMili.time();
+//            }
+//            if(RobotValues.WTFAREWEEVENDOING==2){
+//                while ((int)(elMili.time())-start<traj.length) {
+//                    positionError = startEncoderPosition - traj[(int) (elMili.time() - start)][2];
+//                    // Set some cool pid values
+//                    frontLeft.setVelocityPIDFCoefficients(RobotValues.KV, 0, 0, RobotValues.FEED_FORWARD * (traj[(int) (elMili.time() - start + 5)][1]));
+////                                         dashboardTelemetry.addData("frontLeft Speeed", frontLeft.getVelocity());
+//                    dashboardTelemetry.addData("What it should be ", speed);
+//                    dashboardTelemetry.addData("Velocity error",speed-frontLeft.getVelocity());
+//                    dashboardTelemetry.update();
+//
+//                    speed = traj[(int) (elMili.time() - start)][1];
+//                    telemetry.addData("velocity", speed);
+//                    telemetry.update();
+//                    //               frontRight.setVelocity(speed);
+//                    frontLeft.setVelocity(-speed);
+////                    backRight.setVelocity(speed);
+////                    backLeft.setVelocity(-speed);
+//                }
+//            }
+            }
+        }
 
-                    speed = traj[(int) (elMili.time() - start)][1];
-                    telemetry.addData("velocity", speed);
-                    telemetry.update();
-     //               frontRight.setVelocity(speed);
-                    frontLeft.setVelocity(-speed);
-//                    backRight.setVelocity(speed);
-//                    backLeft.setVelocity(-speed);
-                }
-            }}
+
     }
-    
 
 
-}
+
+
 
 
